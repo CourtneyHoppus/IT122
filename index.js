7423c9830161baaae6a3f18d0c9d59f5b2f03320
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-04-20 13:40:10 -0700

    changed over to express
    this is based on the sample code supplied for class, added views with handlebars and a public folder with css

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 'use strict'
 
-import http from 'http';
-import { parse } from 'querystring';
 import * as myCats from './data.js';
 
-http.createServer((req, res) => {
-  let url = req.url.split("?");
-  let query = parse(url[1]);
-  let path = url[0].toLowerCase();
+import express from 'express';
+import handlebars from 'express-handlebars';
 
-  switch(path) {
-    case '/':
-      let cats = myCats.getAll();
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write('Home page - This is the beginning of my app for IT122' + '\n' +
-                'Here are my cats: ' + '\n');
-      let results = (cats) ? JSON.stringify(cats) : "Where are the cats?.";
-      res.write(results);
-      res.end();
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page - I like cats and have had a few!' + '\n' +
-              'Go to localhost:3000/details followed by ?name= and take a guess!');
-      break;
-    case '/detail':
-      let cat = myCats.getItem(query.name);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let result = (cat) ? JSON.stringify(cat) : "Didn't have that cat.";
-      res.end('Cat ' + query.name + ": " + result);
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found, 404.');
-      break;
-  }
-}).listen(process.env.PORT || 3000);
+const app = express();
+
+app.set('port', 3000);
+app.use(express.static('./public'));
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main.handlebars' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    cats: myCats.getAll()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('home', {
+    body: `About page - I like cats and have had a few!
+      Go to localhost:3000/details followed by ?name= and take a guess!
+      Or back to the home page to enter in the search bar.`
+  });
+});
+
+app.get('/detail', (req, res) => {
+  let cat = myCats.getItem(req.query.name);
+  res.render('details', {
+    name: req.query.name,
+    result: cat
+  });
+});
+
+app.use((req, res) => {
+  res.render('home', {
+    body: '404 - Sorry, Not Found.'
+  });
+});
+
+app.listen(app.get('port'));
